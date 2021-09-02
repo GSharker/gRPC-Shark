cef51b7b61773fbab7412c79440985be33646918
Cesare Caoduro <Cesare.Caoduro@aecom.com>
2021-09-02 18:10:22 +1000

    initiated server with LineService

---
+++ src/server/Geometry/LineService.cs
@@ -0,0 +1,29 @@
+﻿using gRPC.Geometry;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace gRPCShark
+{
+    public class LineService : Line.LineBase
+    {
+        private readonly ILogger<LineService> _logger;
+        public LineService(ILogger<LineService> logger)
+        {
+            _logger = logger;
+        }
+
+        public async override Task<LineResult> GetLine(LineRequest request, ServerCallContext context)
+        {
+            GShark.Geometry.Point3 ps = new GShark.Geometry.Point3(request.StartPoint.X, request.StartPoint.Y, request.StartPoint.Z);
+            GShark.Geometry.Point3 pe = new GShark.Geometry.Point3(request.EndPoint.X, request.EndPoint.Y, request.EndPoint.Z);
+            GShark.Geometry.Line l = new GShark.Geometry.Line(ps, pe);
+            return new LineResult
+            {
+                StartPoint = request.StartPoint,
+                EndPoint = request.EndPoint,
+                Length = l.Length
+            };
+        }
+    }
+}
--- src/server/Startup.cs
+++ src/server/Startup.cs
@@ -28,12 +28,11 @@ namespace gRPC_Shark
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGrpcService<LineService>();
-
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
                 });
+                endpoints.MapGrpcService<LineService>();
             });
         }
     }
