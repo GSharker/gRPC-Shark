3ce9f900988361d9420f38ed9b804170aaf88498
Cesare Caoduro <Cesare.Caoduro@aecom.com>
2021-09-03 12:54:33 +1000

    added client

--- src/client/Program.cs
+++ src/client/Program.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using gRPC.Geometry;
+using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
 
 namespace grpcSharkClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Line.LineClient(channel);
+
+            var reply = await client.GetLineAsync(
+                new LineRequest()
+                {
+                    StartPoint = new gRPC.Geometry.Point3() { X = 0.0, Y = 0.0, Z = 0.0 },
+                    EndPoint = new gRPC.Geometry.Point3() { X = 10, Y = 10, Z = 10 }
+                });
+            Console.WriteLine($"Line length = {reply.Length}");
+            Console.ReadKey();
         }
     }
 }
